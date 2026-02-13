7aab0f40737cf9e8a58a202d53d6d4ae502fba6d
cultustresnadi <48874784+cultustresnadi@users.noreply.github.com>
2026-02-13 16:52:22 +0700

    Add files via upload

---
+++ js/include.js
@@ -0,0 +1,10 @@
+<script>
+	function loadHTML(id, file) {
+	  fetch(file)
+	    .then(res => res.text())
+	    .then(html => document.getElementById(id).innerHTML = html);
+	}
+
+	loadHTML('navbar', '/partials/navbar.html');
+	loadHTML('footer', '/partials/footer.html');
+</script>
