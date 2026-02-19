8bd473c19e3e04c531605b593218d4d4c0b45d65
cultustresnadi <48874784+cultustresnadi@users.noreply.github.com>
2026-02-19 20:06:31 +0700

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,12 +1,83 @@
-document.addEventListener("DOMContentLoaded", function () {
-	function loadHTML(id, file) {
-	  fetch(file)
-	    .then(res => res.text())
-	    .then(html => document.getElementById(id).innerHTML = html);
-	}
+$(document).ready(function () {
 
-	loadHTML('navbar', 'partials/navbar.html');
-	loadHTML('footer', 'partials/footer.html');
-	
+  /* =========================
+     LOAD PARTIAL HTML
+  ========================== */
+  function loadHTML(id, file) {
+    $("#" + id).load(file);
+  }
+
+  //loadHTML('navbar', 'partials/navbar.html');
+  loadHTML('navbar', 'partials/navbarfixed.html');
+  loadHTML('footer', 'partials/footer.html');
+
+
+  /* =========================
+     INIT AOS
+  ========================== */
+  if (typeof AOS !== "undefined") {
+    AOS.init({
+      duration: 900,
+      mirror: true,
+      once: false
+    });
+  }
+
+
+  /* =========================
+     TRACKING FUNCTIONS
+  ========================== */
+
+  window.trackCTA = function (label) {
+    if (typeof gtag !== "undefined") {
+      gtag('event', 'click_whatsapp', {
+        event_category: 'CTA',
+        event_label: label
+      });
+    }
+  };
+
+  window.trackWhatsApp = function (source) {
+    if (typeof gtag !== "undefined") {
+      gtag('event', 'click_whatsapp', {
+        event_category: 'engagement',
+        event_label: source
+      });
+    }
+  };
+
+  /* =========================
+     AUTO CLOSE NAVBAR (Mobile)
+  ========================== */
+  $(document).on('click', '.navbar-collapse .nav-link', function () {
+
+    var navbarCollapse = $('.navbar-collapse');
+    var bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse[0]);
+
+    if (bsCollapse) {
+      bsCollapse.hide();
+    }
+
+  });
+  
+  $(window).on("scroll", function() {
+    
+    if ($(this).scrollTop() > 50) {
+      $("#mainNavbar").addClass("scrolled");
+    } else {
+      $("#mainNavbar").removeClass("scrolled");
+    }
+
+  });
+
+
+  /* =========================
+     FORMAT RUPIAH
+  ========================== */
+  function formatRupiah(value) {
+    if (!value) return "-";
+    return "Rp " + value.toLocaleString("id-ID");
+  }
+
 });
 
